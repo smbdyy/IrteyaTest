0e04438f633e453559d92ac269caf920badc7bf9
Ilya <ividivi67@gmail.com>
2025-11-19 21:33:47 +0300

    configure options, get data from web api

--- Weather/BlazorWeather/Program.cs
+++ Weather/BlazorWeather/Program.cs
@@ -1,3 +1,4 @@
+using BlazorWeather;
 using BlazorWeather.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpWeatherForecastSource(builder.Configuration);
 
 var app = builder.Build();
 
